3c196f944997bf0770dcd064582196baadeebc22
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-15 23:11:57 +0300

    Add files via upload

---
+++ events.h
@@ -0,0 +1,23 @@
+//
+// Created by Duru Irmak Öztürk on 26.02.2024.
+//
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+#include "spaceships.h"
+#include "commonship2.h"
+#include "speedship2.h"
+#include "strongship2.h"
+#ifndef UNTITLED_EVENTS_H
+#define UNTITLED_EVENTS_H
+class Events {
+public:
+    //GAME STARTER
+    static void start_game();
+
+    //SHIP SELECTOR
+    static std::shared_ptr<Spaceships>playersShip();
+
+}
+#endif //UNTITLED_EVENTS_H
